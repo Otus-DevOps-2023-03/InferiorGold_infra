8c772f3ca2383b5aa580a7b499db2fce3196af7c
Владислав Вахитов <vvakhitov@MacBook-Air-Vladislav.local>
2023-04-27 21:47:33 +0300

    HW6 fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 apt-get install apt-transport-https ca-certificates
-apt remove --autoremove mongodb-org
-rm /etc/apt/sources.list.d/mongodb*.list
-apt update
 echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 sudo apt-get update  && sudo apt-get upgrade -y
 sudo apt --fix-broken install
-apt remove mongodb
-apt purge mongodb
-apt autoremove
 apt-get install -y mongodb-org
+apt-get update
 #Start mongodb
 systemctl enable mongod
 systemctl start mongod
 systemctl status mongod
+mongo --version
