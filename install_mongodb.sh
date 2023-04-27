1242a828df51cd24f133983f5e0edbaa58845651
InferiorGold <v.vahitov@gmail.com>
2023-04-27 22:35:53 +0300

    HW6 fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 apt install apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B00A0BD1E2C63C11
-echo "deb [arch=amd64] http://repo.mongodb.org/apt/ubuntu $(lsb_release -sc)/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 apt update  && apt upgrade -y
 #apt --fix-broken install
 #apt update
