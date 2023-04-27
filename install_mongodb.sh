2ef01d9ecbd5cb992c854fb05544eee1f941a3db
InferiorGold <v.vahitov@gmail.com>
2023-04-27 22:31:59 +0300

    HW6 fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -9,11 +9,15 @@ apt update  && apt upgrade -y
 apt install mongodb -y
 apt update
 #Start mongodb
-systemctl enable mongodb.service
-systemctl start mongodb.service
-systemctl status mongodb.service
-systemctl enable mongod.service
-systemctl start mongod.service
-systemctl status mongod.service
+systemctl enable mongodb
+systemctl start mongodb
+systemctl status mongodb
+service mongod enable
+service mongod start
+service mongod status
+systemctl enable mongod
+systemctl start mongod
+systemctl status mongod
+nc -zvv localhost 27017
 mongo --version
 mongod --version
