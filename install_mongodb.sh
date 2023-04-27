3a602fa5a7347ca74ec4c50c53234a0f2d9b2fdb
Vladislav Vakhitov <v.vahitov@gmail.com>
2023-04-27 18:12:37 +0300

    fix files for HW06

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# sudo apt-get install apt-transport-https ca-certificates
-#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get install apt-transport-https ca-certificates
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 sudo apt install -y mongodb
-
+sudo apt update
+sudo apt install -y mongodb-org
 #Start mongodb
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
 sudo systemctl status mongodb
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
