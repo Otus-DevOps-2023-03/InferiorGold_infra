e3ca4905d1e22e7a801ebbefe9b2ca54a4258332
Владислав Вахитов <vvakhitov@MacBook-Air-Vladislav.local>
2023-04-27 22:12:18 +0300

    HW6 fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #enter in home directory
-cd /home/yc-user
+#cd /home/yc-user
 #clone repository app from git
 git clone -b monolith https://github.com/express42/reddit.git
 #enter in folder and install app
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-apt-get install apt-transport-https ca-certificates
-echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-sudo apt-get update  && sudo apt-get upgrade -y
-sudo apt --fix-broken install
-apt-get install -y mongodb
-apt-get update
+apt install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv B00A0BD1E2C63C11
+echo "deb [arch=amd64] http://repo.mongodb.org/apt/ubuntu $(lsb_release -sc)/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
+#wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+apt update  && apt upgrade -y
+apt --fix-broken install
+apt update
+apt install mongodb-org
+apt update
 #Start mongodb
-systemctl enable mongod
-systemctl start mongod
-systemctl status mongod
+systemctl enable mongodb.service
+systemctl start mongodb.service
+systemctl status mongodb.service
+systemctl enable mongod.service
+systemctl start mongod.service
+systemctl status mongod.service
 mongo --version
+mongod --version
