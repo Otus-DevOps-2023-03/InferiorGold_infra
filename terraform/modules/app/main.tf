68a6badf1d270b5cac98a5e1b58a539bba7a576e
Vladislav Vakhitov <v.vahitov@gmail.com>
2023-05-05 18:07:56 +0300

    HW-09

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.89.0"
+    }
+  }
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+
+  labels = {
+    tags = "reddit-app"
+  }
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.app_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+}
+
+
+resource "null_resource" "app" {
+
+  connection {
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface[0].nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = var.private_key_path
+  }
+
+
+  provisioner "file" {
+    #source      = "${path.module}/files/puma.service"
+    content     = templatefile("${path.module}/files/puma.service", { db_ip = var.db_ip })
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/files/deploy.sh"
+  }
+}
--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -1,9 +1,3 @@
-variable "cloud_id" {
-  description = "Cloud"
-}
-variable "folder_id" {
-  description = "Folder"
-}
 variable "zone" {
   description = "Zone"
   # Значение по умолчанию
@@ -13,21 +7,14 @@ variable "public_key_path" {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable "image_id" {
-  description = "Disk image"
-}
 variable "subnet_id" {
   description = "Subnet"
 }
-variable "service_account_key_file" {
-  description = "key .json"
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
 variable "private_key_path" {
   # Описание переменной
   description = "Path to the private key used for ssh access"
-}
-variable "count_app" {
-  description = "app"
-  # Значение по умолчанию
-  default = 1
 }
